aff2ca91b359933780c714f986b9338141e0b940
mattjp <mattjp@umich.edu>
2017-08-05 15:35:42 -0400

    added upload functionality

--- vanilla/vanilla.py
+++ vanilla/vanilla.py
@@ -2,6 +2,8 @@ import os, sqlite3, re, flask_login
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from flask_bcrypt import Bcrypt
+from werkzeug.utils import secure_filename
+
 
 # Data types ###################################################################
 class User(flask_login.UserMixin):
@@ -10,6 +12,7 @@ class User(flask_login.UserMixin):
 
 	def get_id(self):
 		return (self.id[0], self.id[1])
+
 
 # Application setup ############################################################
 app = Flask(__name__)
@@ -26,7 +29,8 @@ app.config.update(dict(
 ))
 app.config.from_envvar('VANILLA_SETTINGS', silent = True)
 
-# Database functions ###########################################################
+
+# Database Functions ###########################################################
 def connect_db():
 	rv = sqlite3.connect(app.config['DATABASE'])
 	rv.row_factory = sqlite3.Row
@@ -37,11 +41,6 @@ def init_db():
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    init_db()
-    print('Initialized the database.')
 
 def get_db():
     if not hasattr(g, 'sqlite_db'):
@@ -70,6 +69,8 @@ def insert(table, fields = (), values = ()):
 	cur.commit()
 	cur.close()
 
+
+# Command Line Functions #######################################################
 @app.cli.command('insert-vendor')
 def insert_vendor_command():
 	v_name = input('Vendor Name: ')
@@ -77,6 +78,12 @@ def insert_vendor_command():
 	v_em = input('Vendor Email: ' )
 	insert('vendors', ['vendorName', 'password', 'email'], [v_name, v_pw, v_em])
 	print('Added', v_name, 'to vendors.')
+
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print('Initialized the database.')
+
 
 # User Functions ###############################################################
 @login_manager.user_loader
@@ -87,73 +94,88 @@ def complete_login(id, cur_type):
 	session['logged_in'] = True
 	flask_login.login_user(User(id, cur_type))
 
-# View functions ###############################################################
+
+# View Functions - Home ########################################################
 @app.route('/')
 def show_home():
 	return render_template('home.html')
 
-@app.route('/sample_vendor')
-def sample_vendor():
-	return render_template('sample_vendor.html')
-
 @app.route('/', methods = ['GET', 'POST'])
 def login():
 	error = None
-	# Handle login POST requests
 	if request.method == 'POST' and request.form['action'] == 'login':
 		req_em = request.form['email']
 		req_pw = request.form['password']
-		# Query both databases
 		user = query_db('select * from users where email = ?', [req_em], True)
 		vendor = query_db('select * from vendors where email = ?', [req_em], True)
-		# If the user exists
 		if user:
 			if bcrypt.check_password_hash(user['password'], req_pw):
 				complete_login(req_em, 'user')
 				return render_template('home.html')
 			else:
 				error = 'Incorrect password for supplied email'
-		# If the vendor exists
 		elif vendor:
 			if bcrypt.check_password_hash(vendor['password'], req_pw):
 				complete_login(vendor['vendorName'], 'vendor')
 				return render_template('home.html')
 			else:
 				error = 'wrong vendor password'
-		# Email doesn't exist in either table
 		else:
 			error = 'Email does not exist in either database'
-
-	# Handle sign up POST requests
 	elif request.method == 'POST' and request.form['action'] == 'signup':
 		req_em = request.form['email']
 		req_pw = request.form['password']
-		# Assert email form is valid
 		valid_form = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
 		valid_em = re.match(valid_form, req_em)
-		# Throw error if email is not valid
 		if valid_em == None:
 			error = 'SIGN-IN: Not a valid email address'
-		# Check to see if a password was entered
 		elif req_pw:
-			# Query both databases
 			user = query_db('select * from users where email = ?', [req_em], True)
 			vendor = query_db('select * from vendors where email = ?', [req_em], True)
-			# Create new user if the email doesn't exist in either table 
 			if not user and not vendor:
-				# Hash & salt password, store new user in table
 				enc_pw = bcrypt.generate_password_hash(req_pw)
 				insert('users', ['password', 'email'], [enc_pw, req_em])
 				complete_login(req_em, 'user')
 				return render_template('home.html')
 			else:
 				error = 'Email already exists in database'
-		# Email was valid, but no password was supplied
 		else:
 			error = 'No password supplied'
-	# Render home screen for non-valid POST requests
 	return render_template('home.html', error = error)
 
+# Add item to vendor page
+def add_item(item_name, item_desc, item_img, vendor):
+	folder = 'assets/' + vendor + '/'
+	app.config['UPLOAD_FOLDER'] = folder
+	fname = secure_filename(item_img.filename)
+	full_path = folder + fname
+	item_img.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
+	insert('items', ['itemName', 'description', 'vendor', 'pathToImg'], \
+		[item_name, item_desc, vendor, full_path])
+
+# View Functions - Sample ######################################################
+@app.route('/sample_vendor')
+def show_sample_vendor():
+	return render_template('sample_vendor.html')
+
+@app.route('/sample_vendor', methods = ['GET', 'POST'])
+def sample_vendor_add():
+	if request.method == 'POST':
+		item_name = request.form['itemName']
+		item_desc = request.form['itemDesc']
+		item_img = request.files['file']
+		app.config['UPLOAD_FOLDER'] = 'assets/sample_vendor/'
+		fname = secure_filename(item_img.filename)
+		full_path = 'assets/sample_vendor/' + fname
+		item_img.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
+		insert('items', ['itemName', 'description', 'vendor', 'pathToImg'], \
+			[item_name, item_desc, 'Dog Flat ', full_path])
+	return redirect(url_for('show_sample_vendor'))
+
+# View Functions - Store2 ######################################################
+
+
+# View Functions - Logout ######################################################
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
