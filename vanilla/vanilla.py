b3db51b764f5dee0fbcb7f954ada2e53f29bdb64
mattjp <mattjp@umich.edu>
2017-08-12 12:38:36 -0400

    backend emailing, changed request handling

--- vanilla/vanilla.py
+++ vanilla/vanilla.py
@@ -2,7 +2,10 @@ import os, sqlite3, re, flask_login
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from flask_bcrypt import Bcrypt
+from flask_mail import Mail, Message
+# from flask_emails import Message
 from werkzeug.utils import secure_filename
+from collections import defaultdict, OrderedDict
 
 
 # Data types ###################################################################
@@ -25,9 +28,17 @@ login_manager.init_app(app)
 
 app.config.update(dict(
 	DATABASE = os.path.join(app.root_path, 'vanilla.db'),
-	SECRET_KEY = 'development key' # TODO change me!
+	SECRET_KEY = 'development key', # TODO change me!
+	MAIL_SERVER = 'smtp.gmail.com',
+	MAIL_PORT = 587,
+	MAIL_USE_TLS = True,
+    MAIL_USE_SSL = False,
+    MAIL_USERNAME = 'mjperetick@gmail.com',
+    MAIL_PASSWORD = '' # Not putting this on GitHub
 ))
 app.config.from_envvar('VANILLA_SETTINGS', silent = True)
+
+mail = Mail(app)
 
 
 # Database Functions ###########################################################
@@ -110,16 +121,59 @@ def complete_login(id, cur_type):
 	flask_login.login_user(User(id, cur_type))
 
 
-# View Functions - Home ########################################################
-@app.route('/')
-def show_home():
-	items = query_db('select * from vendors')
-	return render_template('home.html', items = items)
+# Helper Functions #############################################################
+# Remove item from vendor page
+def delete_item(item_name, vendor):
+	path = query_db('select * from items where vendor = ? and itemName = ?', \
+		[vendor, item_name], True)['pathToImg'][3:]
+	print(path)
+	os.remove(path)
+	delete_from_db('delete from items where vendor = ? and itemName = ?', \
+		[vendor, item_name])
 
-@app.route('/', methods = ['GET', 'POST'])
-def login():
+# Add item to vendor page
+def add_item(item_name, item_desc, item_img, vendor, item_price):
+	exists = query_db('select * from items where itemName = ?', [item_name])
+	# TODO this only kind of work
+	if exists:
+		print('CANNOT HAVE ITEMS WITH THE SAME NAME')
+		site = vendor + '.html'
+		return render_template(site, error = 'cannot have item with same name')
+	folder = 'static/' + vendor + '/'
+	app.config['UPLOAD_FOLDER'] = folder
+	fname = secure_filename(item_img.filename)
+	full_path = '../' + folder + fname
+	item_img.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
+	insert('items', ['itemName', 'description', 'vendor', 'pathToImg', 'price'], \
+		[item_name, item_desc, vendor, full_path, item_price])
+
+# Query the vendors database, display all vendors alphabetically
+def find_all_vendors():
+	all_vendors = query_db('select * from vendors')
+	vendor_list = defaultdict(list)
+	letters = []
+	for vendor in all_vendors:
+		if vendor['displayName'][0] not in letters:
+			letters.append(vendor['displayName'][0])
+	for vendor in all_vendors:
+		vendor_list[vendor['displayName'][0]].append((vendor['vendorName'], \
+			vendor['displayName']))
+	for letter, vendor in vendor_list.items():
+		vendor_list[letter.lower()] = vendor_list.pop(letter)
+	vendor_list = OrderedDict(sorted((vendor_list).items()))
+	for letter, vendor in vendor_list.items():
+		vendor = vendor.sort()
+	return vendor_list
+
+# Sends an email
+def send_email(subject, body):
+	mail.send_message(subject = subject, body = body, \
+		recipients = ['mjperetick@gmail.com'], \
+		sender = ('Verdeckt', 'mjperetick@gmail.com'))
+
+# Handles all requests that can come from the NavBar
+def request_handler(request):
 	error = None
-	items = query_db('select * from vendors')
 	if request.method == 'POST' and request.form['action'] == 'login':
 		req_em = request.form['email']
 		req_pw = request.form['password']
@@ -128,13 +182,13 @@ def login():
 		if user:
 			if bcrypt.check_password_hash(user['password'], req_pw):
 				complete_login(req_em, 'user')
-				return render_template('home.html', items = items)
+				return error
 			else:
 				error = 'Incorrect password for supplied email'
 		elif vendor:
 			if bcrypt.check_password_hash(vendor['password'], req_pw):
 				complete_login(vendor['vendorName'], 'vendor')
-				return render_template('home.html', items = items)
+				return error
 			else:
 				error = 'wrong vendor password'
 		else:
@@ -153,7 +207,7 @@ def login():
 				enc_pw = bcrypt.generate_password_hash(req_pw)
 				insert('users', ['password', 'email'], [enc_pw, req_em])
 				complete_login(req_em, 'user')
-				return render_template('home.html', items = items)
+				return error
 			else:
 				error = 'Email already exists in database'
 		else:
@@ -167,32 +221,32 @@ def login():
 		else:
 			update_db('update vendors set password = ? where vendorName = ?', \
 				[new_pw, user_id])
-	return render_template('home.html', error = error, items = items)
+	elif request.method == 'POST' and request.form['action'] == 'email':
+		send_email(request.form['email_subject'], request.form['email_content'])
+	return error
+		
 
-# Add item to vendor page
-def add_item(item_name, item_desc, item_img, vendor, item_price):
-	exists = query_db('select * from items where itemName = ?', [item_name])
-	# TODO this only kind of work
-	if exists:
-		print('CANNOT HAVE ITEMS WITH THE SAME NAME')
-		site = vendor + '.html'
-		return render_template(site, error = 'cannot have item with same name')
-	folder = 'static/' + vendor + '/'
-	app.config['UPLOAD_FOLDER'] = folder
-	fname = secure_filename(item_img.filename)
-	full_path = '../' + folder + fname
-	item_img.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
-	insert('items', ['itemName', 'description', 'vendor', 'pathToImg', 'price'], \
-		[item_name, item_desc, vendor, full_path, item_price])
+# View Functions - Home ########################################################
+@app.route('/')
+def show_home():
+	return render_template('home.html')
 
-# Remove item from vendor page
-def delete_item(item_name, vendor):
-	path = query_db('select * from items where vendor = ? and itemName = ?', \
-		[vendor, item_name], True)['pathToImg'][3:]
-	print(path)
-	os.remove(path)
-	delete_from_db('delete from items where vendor = ? and itemName = ?', \
-		[vendor, item_name])
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+	error = request_handler(request)
+	return render_template('home.html', error = error)
+
+
+# View Functions - Brands ######################################################
+@app.route('/brands')
+def show_all_brands():
+	items = find_all_vendors()
+	return render_template('brands.html', items = items)
+
+@app.route('/brands', methods = ['GET', 'POST'])
+def brands_request():
+	error = request_handler(request)
+	return redirect(url_for('show_all_brands')) # TODO: pass back the error somehow
 
 
 # View Functions - Sample ######################################################
@@ -205,6 +259,7 @@ def show_represent_clo():
 @app.route('/represent_clo', methods = ['GET', 'POST'])
 def update_represent_clo():
 	STORE = 'represent_clo'
+	error = request_handler(request)
 	if request.method == 'POST' and flask_login.current_user.id[0] == STORE:
 		if request.form['action'] == 'add_item':
 			add_item(request.form['itemName'], request.form['itemDesc'], \
@@ -240,16 +295,7 @@ def logout():
     flask_login.logout_user()
     return redirect(url_for('show_home'))
 
-
+# Run Function #################################################################
 if __name__ == '__main__':
 	app.run()
-
-
-
-
-
-
-
-
-
 
